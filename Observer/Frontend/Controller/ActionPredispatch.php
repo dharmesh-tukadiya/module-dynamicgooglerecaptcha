4c61c4953893692e12ca6712d764a0db93256cfc
Dharmesh N Tukadiya <140082778+dharmesh-tukadiya@users.noreply.github.com>
2024-05-13 18:02:13 +0530

    Update ActionPredispatch.php

--- Observer/Frontend/Controller/ActionPredispatch.php
+++ Observer/Frontend/Controller/ActionPredispatch.php
@@ -17,6 +17,7 @@ use Magento\ReCaptchaUi\Model\RequestHandlerInterface;
 use Magento\Framework\App\Response\RedirectInterface;
 use DnTukadiya\DynamicGoogleReCaptcha\Model\ResourceModel\DynamicCaptcha\CollectionFactory;
 use \Magento\Framework\App\Config\ScopeConfigInterface;
+use \Magento\Framework\App\CacheInterface;
 
 class ActionPredispatch implements ObserverInterface
 {
@@ -27,12 +28,15 @@ class ActionPredispatch implements ObserverInterface
     private $collectionFactory;
     private $scopeConfig;
     private $moduleStatus;
+    private $cache;
+
     public function __construct(
         IsCaptchaEnabledInterface $isCaptchaEnabled,
         RequestHandlerInterface $requestHandler,
         RedirectInterface $redirect,
         CollectionFactory $collectionFactory,
-        ScopeConfigInterface $scopeConfig
+        ScopeConfigInterface $scopeConfig,
+        CacheInterface $cache
     ) {
         $this->isCaptchaEnabled = $isCaptchaEnabled;
         $this->requestHandler = $requestHandler;
@@ -40,6 +44,7 @@ class ActionPredispatch implements ObserverInterface
         $this->collectionFactory = $collectionFactory;
         $this->scopeConfig = $scopeConfig;
         $this->moduleStatus = $this->scopeConfig->getValue('dntukadiya_dynamicgooglerecaptcha/module/status');
+        $this->cache = $cache;
     }
 
     public function execute(Observer $observer): void
@@ -47,20 +52,28 @@ class ActionPredispatch implements ObserverInterface
         if ($this->moduleStatus) {
             $key = false;
             $request = $observer->getRequest();
-            $collection = $this->collectionFactory->create();
-            $collection->addFieldToFilter('submit_controller_path', [trim($request->getOriginalPathInfo() ?? ""), trim(rtrim($request->getOriginalPathInfo() ?? "", '/')), trim(ltrim($request->getOriginalPathInfo() ?? "", '/'))]);
-            $collection->addFieldToFilter('action_method', $request->getMethod());
-            $collection->addFieldToFilter('status', 1);
-            if ($collection->count()) {
-                $key = $collection->getFirstItem()->getIdentifier() . \DnTukadiya\DynamicGoogleReCaptcha\Model\DynamicCaptchaConfigResolver::CONFIG_SEPARATOR;
+            $controllerPaths = [trim($request->getOriginalPathInfo() ?? ""), trim(rtrim($request->getOriginalPathInfo() ?? "", '/')), trim(ltrim($request->getOriginalPathInfo() ?? "", '/'))];
+            $cacheKey = 'dynamicgooglerecaptcha_' . md5(json_encode($controllerPaths));
+            $key = $this->cache->load($cacheKey);
+            if (empty($key)) {
+                $collection = $this->collectionFactory->create();
+                $collection->addFieldToFilter('submit_controller_path', $controllerPaths);
+                $collection->addFieldToFilter('action_method', $request->getMethod());
+                $collection->addFieldToFilter('status', 1);
+                if ($collection->count()) {
+                    $key = $collection->getFirstItem()->getIdentifier() . \DnTukadiya\DynamicGoogleReCaptcha\Model\DynamicCaptchaConfigResolver::CONFIG_SEPARATOR;
+                }
             }
-            if ($key && $this->isCaptchaEnabled->isCaptchaEnabledFor($key)) {
+            if ($key && $key != "0" && $this->isCaptchaEnabled->isCaptchaEnabledFor($key)) {
+                $this->cache->save($key, $cacheKey, [], null);
                 /** @var Action $controller */
                 $controller = $observer->getControllerAction();
                 $request = $controller->getRequest();
                 $response = $controller->getResponse();
                 $redirectOnFailureUrl = $this->redirect->getRedirectUrl();
                 $this->requestHandler->execute($key, $request, $response, $redirectOnFailureUrl);
+            } else {
+                $this->cache->save(0, $cacheKey, [], null);
             }
         }
     }
